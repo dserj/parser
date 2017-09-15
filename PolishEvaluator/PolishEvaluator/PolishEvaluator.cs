18d6fa00c7d0e27d1336de2c2029e43fc6afcc54
serj <serj@serj-w81.msk.bonasource.ru>
2017-09-15 16:53:43 +0300

    evaluator

---
+++ PolishEvaluator/PolishEvaluator/PolishEvaluator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolishEvaluator
+{
+    using System.IO;
+
+    public class PolishEvaluator
+    {
+        private readonly string input;
+        private readonly Stack<double> stack = new Stack<double>();
+
+        public PolishEvaluator(string input)
+        {
+            this.input = input;
+        }
+
+        public double Evaluate()
+        {
+            foreach (char c in input)
+            {
+                if (IsNumber(c))
+                {
+                    double d;
+                    Double.TryParse(c.ToString(), out d);
+                    stack.Push(d);
+                    continue;
+                }
+
+                if (IsOperator(c))
+                {
+                    // right operator is always on the top of the stack
+                    stack.Push(ApplyOperator(c, stack.Pop(), stack.Pop()));
+                    continue;
+                }
+
+                throw new InvalidDataException("Invalid input data");
+            }
+
+            return stack.Pop();
+        }
+
+        private double ApplyOperator(char op, double rh, double lh)
+        {
+            switch (op)
+            {
+                case '+':
+                {
+                    return lh + rh;
+                }
+                case '-':
+                {
+                    return lh - rh;
+                }
+                case '*':
+                {
+                    return lh * rh;
+                }
+                case '/':
+                {
+                    return lh / rh;
+                }
+
+                default:
+                    throw new InvalidDataException("Unsupported operator");
+            }
+        }
+
+        private bool IsNumber(char ch)
+        {
+            int result;
+            if (int.TryParse(ch.ToString(), out result))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsOperator(char ch)
+        {
+            var ops = new[] { '+', '-', '/', '*' };
+            return ops.Contains(ch);
+        }
+    }
+}
--- SyntaxParserTests/ReversePolishConverterTests.cs
+++ SyntaxParserTests/ReversePolishConverterTests.cs
@@ -3,17 +3,37 @@
 namespace SyntaxParserTests
 {
     using PolishConverter;
+    using PolishEvaluator;
 
     [TestClass]
     public class ReversePolishConverterTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestConverter()
         {
             var converter = new ReversePolishConverter("5+2*3");
             var result = converter.Convert();
 
             Assert.AreEqual(result, "523*+");
+        }
+
+        [TestMethod]
+        public void TetsEvaluator()
+        {
+            var evaluator = new PolishEvaluator("523*+");
+            var result = evaluator.Evaluate();
+
+            Assert.AreEqual(result, 11, string.Format("Expected 11, but got {0}", result));
+        }
+
+        [TestMethod]
+        public void TestCalculator()
+        {
+            var converter = new ReversePolishConverter("5+2*3");
+            var evaluator = new PolishEvaluator(converter.Convert());
+            var result = evaluator.Evaluate();
+
+            Assert.AreEqual(result, 11, string.Format("Expected 11, but got {0}", result));
         }
     }
 }
